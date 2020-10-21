c13eff83351a2137e73761a628436f977ed62e94
Sim0no <german.marin@mail.escuelaing.edu.co>
2020-10-21 14:08:09 -0500

    index

--- src/main/java/edu/eci/arsw/weather/controllers/WeatherStatsController.java
+++ src/main/java/edu/eci/arsw/weather/controllers/WeatherStatsController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/weather")
+@RequestMapping("/api")
 public class WeatherStatsController {
     @Autowired
     private IWeatherStatsService weatherStatsS;
